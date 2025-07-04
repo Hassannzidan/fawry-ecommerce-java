6fee6e608561e3e9f0cfbbb8f6ec6fbc8c8a4bbc
Hassan Mohamed <hzidan014@gmail.com>
2025-07-04 22:31:06 +0300

    Update CheckoutService.java

--- src/main/java/com/fawry/challenge/fawryecommerce/service/CheckoutService.java
+++ src/main/java/com/fawry/challenge/fawryecommerce/service/CheckoutService.java
@@ -10,64 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CheckoutService {
-
-//    public static void checkout(Customer customer, Cart cart) {
-//        if (cart.isEmpty()) {
-//            throw new RuntimeException("Cart is empty");
-//        }
-//
-//        double subtotal = 0;
-//        List<Shippable> shippableItems = new ArrayList<>();
-//
-//        for (CartItem item : cart.getItems()) {
-//            Product product = item.product;
-//
-//            if (product instanceof Expirable) {
-//                if (((Expirable) product).isExpired()) {
-//                    throw new RuntimeException(product.getName() + " is expired");
-//                }
-//            }
-//
-//            if (item.quantity > product.getQuantity()) {
-//                throw new RuntimeException(product.getName() + " is out of stock");
-//            }
-//
-//            subtotal += item.getTotalPrice();
-//
-//            if (product instanceof Shippable) {
-//                for (int i = 0; i < item.quantity; i++) {
-//                    shippableItems.add((Shippable) product);
-//                }
-//            }
-//        }
-//
-//        double shippingFee = shippableItems.isEmpty() ? 0 : 30;
-//        double total = subtotal + shippingFee;
-//
-//        if (customer.getBalance() < total) {
-//            throw new RuntimeException("Insufficient balance");
-//        }
-//
-//        for (CartItem item : cart.getItems()) {
-//            item.product.reduceQuantity(item.quantity);
-//        }
-//
-//        customer.pay(total);
-//
-//        if (!shippableItems.isEmpty()) {
-//            ShippingService.ship(shippableItems);
-//        }
-//
-//        System.out.println("- * Checkout receipt **");
-//        for (CartItem item : cart.getItems()) {
-//            System.out.println(item.quantity + "x " + item.product.getName() + "    " + item.getTotalPrice());
-//        }
-//        System.out.println("------------------------");
-//        System.out.println("Subtotal     " + subtotal);
-//        System.out.println("Shipping     " + shippingFee);
-//        System.out.println("Amount       " + total);
-//        System.out.println("Remaining Balance: " + customer.getBalance());
-//    }
     public static void checkout(Customer customer, Cart cart) {
         if (cart.isEmpty()) {
             throw new RuntimeException("Cart is empty");
